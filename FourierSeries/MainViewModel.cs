34c9b4ce1fae4093cbe981ec94958d41f0c59947
Александр Брусленко <BRUSLENKOAL@gmail.com>
2021-03-03 13:09:15 +0300

    First Upload

---
+++ FourierSeries/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using OxyPlot;
+using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourierSeries
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            Create();
+        }
+
+        public void SetPoints(List<Point> points)
+        {
+            Points.Clear();
+            foreach (Point p in points)
+            {
+                this.Points.Add(new DataPoint(p.X, p.Y));
+            }
+            this.MyModel.ResetAllAxes();
+            this.MyModel.InvalidatePlot(true);
+
+        }
+
+        private void Create()
+        {
+            this.MyModel = new PlotModel { Title = "Ряд Фурье", PlotType = PlotType.Cartesian };
+            this.Points = new List<DataPoint>();
+            MyModel.Series.Add(new LineSeries()
+            {
+                Color = OxyColors.Green,
+                ItemsSource = Points,
+                LineStyle = LineStyle.Solid,
+                StrokeThickness = 1f
+            });
+        }
+
+        public IList<DataPoint> Points { get; private set; }
+
+        public PlotModel MyModel { get; private set; }
+    }
+}
---
+++ FourierSeries/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using org.mariuszgromada.math.mxparser;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Expression = org.mariuszgromada.math.mxparser.Expression;
+
+namespace FourierSeries
+{
+    public partial class MainWindow : Window
+    {
+        List<Point> res = new List<Point>();
+        Expression IntExpression;
+        Expression intAexpresstion;
+        Expression intBexpresstion;
+
+        public double a=-5;
+        public double b=5;
+
+        public int n;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            labelResult.Content = "";
+
+            n = Convert.ToInt32(textBoxN.Text);
+
+            Argument nInt = new Argument("n", n);
+
+            IntExpression = new Expression("int(" + textBox3.Text + ", x, -3.14, 3.14)");
+            intAexpresstion = new Expression("int((" + textBox3.Text + ")*(cos(n*x)), x, -3.14, 3.14)", nInt);
+            intBexpresstion = new Expression("int((" + textBox3.Text + ")*(sin(n*x)), x, -3.14, 3.14)", nInt);
+
+            res.Clear();
+
+
+            FourierSeries fourier = Fourier(n);
+
+            for (double x = a; x < b; x += 0.1)
+            {
+                double fx = 0;
+
+                fx += fourier.a0 / 2;
+
+                for (int i = 0; i < fourier.a.Count; i++)
+                {
+                    fx += (fourier.a[i] * Math.Cos((i + 1) * x))
+                        + (fourier.b[i] * Math.Sin((i + 1) * x));
+                }
+
+                res.Add(new Point(x, fx));
+            }
+
+            CreateBigGraph("Ряд Фурье");
+        }
+
+        public FourierSeries Fourier(int nCount)
+        {
+            FourierSeries fourier = new FourierSeries();
+            fourier.a = new List<double>();
+            fourier.b = new List<double>();
+
+            fourier.a0 = (1 / a) * IntExpression.calculate();
+
+            for (int n = 1; n < nCount + 1; n++)
+            {
+                fourier.a.Add((1 / Math.PI) * Integral(intAexpresstion, n));
+                fourier.b.Add((1 / Math.PI) * Integral(intBexpresstion, n));
+            }
+
+            return fourier;
+        }
+
+        public struct FourierSeries
+        {
+            public double a0;
+            public List<double> a;
+            public List<double> b;
+        }
+
+        private double Integral(Expression exp, double n)
+        {
+            exp.setArgumentValue("n", n);
+            return exp.calculate();
+        }
+
+        private void CreateBigGraph(string name)
+        {
+            (this.DataContext as MainViewModel).SetPoints(res);
+            (this.DataContext as MainViewModel).MyModel.Title = name;
+        }
+    }
+}
